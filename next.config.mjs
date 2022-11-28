ac10df64e3bc37fcb20c33224012d3ae68535301
appeL <ginodaloisio@icloud.com>
2022-11-28 20:07:59 -0300

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,31 @@
+// @ts-check
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
+ * This is especially useful for Docker builds.
+ */
+!process.env.SKIP_ENV_VALIDATION && (await import("./src/env/server.mjs"));
+
+/** @type {import("next").NextConfig} */
+const config = {
+  reactStrictMode: true,
+  swcMinify: true,
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+  // future: {
+  //   webpack5: true, // by default, if you customize webpack config, they switch back to version 4.
+  //   // Looks like backward compatibility approach.
+  // },
+  webpack(config) {
+    config.resolve.fallback = {
+      ...config.resolve.fallback, // if you miss it, all the other options in fallback, specified
+      // by next.js will be dropped. Doesn't make much sense, but how it is
+      fs: false, // the solution
+    };
+
+    return config;
+  },
+  pageExtensions: ["proto", "js", "tsx", "ts"],
+};
+export default config;
